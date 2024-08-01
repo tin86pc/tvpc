04660ff33e24dfff23515725e0f8a30a9c1b5f3a
tin86pc <tin86pc@gmail.com>
2024-08-01 17:46:57 +0700

    1.8

---
+++ func.js
@@ -0,0 +1,26 @@
+function doc(key) {
+
+    // đọc
+    const S_obj = localStorage.getItem("tvpc");
+    let obj = new Object();
+    if (S_obj != null) {
+        Object.assign(obj, JSON.parse(S_obj));
+    }
+    return obj[key];
+}
+
+function ghi(o) {
+    // đọc
+    const S_obj = localStorage.getItem("tvpc");
+    let obj = new Object();
+    if (S_obj != null) {
+        Object.assign(obj, JSON.parse(S_obj));
+    }
+
+    // ghi 
+    Object.assign(obj, o);
+    localStorage.setItem("tvpc", JSON.stringify(obj));
+    console.log(obj);
+}
+
+export { doc, ghi }
